1389db8c635b315ef7947b0e805deb34977ea5a3
Willem Serruys <serruys.willem@hotmail.com>
2019-07-05 10:24:06 +0200

    Cleanup

--- I2CSlave/I2CSlave.ino
+++ I2CSlave/I2CSlave.ino
@@ -2,91 +2,86 @@
 #include <SPI.h>
 #include <MCP4922.h>
 
-const int dacChipSelectPin1 = 7;
-const int dacChipSelectPin2 = 8;
 
+// 2 DACs
+MCP4922 DAC1(16,15,7,14);
+MCP4922 DAC2(16,15,8,14);
 
-long minimum = 0;
-long maximum = 255;
-
-
+//i2c communication setup with RPi
 #define SLAVE_ADDRESS 0x08
+
+// used to map incoming values to DAC values
+int minimum = 0;
+int maximum = 255;
+
+int dacMinimum = 0;
+int dacMaximum = 4095;
+
+//Values that will be read from RPi
 int cmd = 0;
 int steerPosition = 0;
 int throttlePosition = 0;
 int aux1Position = 0;
 int aux2Position = 0;
 
+// Store previous values as well; set DAC only when difference
 int prevSteerPosition = 0;
 int prevThrottlePosition = 0;
 int prevAux1Position = 0;
 int prevAux2Position = 0;
 
+// Threshold for data change (0-4095)
+int dataThreshold = 10;
+
+
 
 void setup() {
-  Serial.begin(9600); // start serial for output
-  // initialize i2c as slave
+  Serial.begin(9600);
   Wire.begin(SLAVE_ADDRESS);
-  
-  // define callbacks for i2c communication
   Wire.onReceive(receiveData);
-  
-  
-  Serial.println("Ready!");
   SPI.begin();
 }
 
-void loop() 
-{
-  setDac(0,0,dacChipSelectPin1);
-  setDac(0,1,dacChipSelectPin1);
-  delay(10);
-//  if (dataChanged(prevSteerPosition, steerPosition))
-//  {
-//    setDac(steerPosition,0,dacChipSelectPin1);
-//    Serial.println("SP changed");
-//    delay(10);
-//  }
-//  if (dataChanged(prevThrottlePosition, throttlePosition))
-//  {
-//    setDac(throttlePosition,1,dacChipSelectPin1);
-//    Serial.println("TP changed");
-//    delay(10);
-//  }
-//  if (dataChanged(prevAux1Position, aux1Position))
-//  {
-//    setDac(aux1Position,0,dacChipSelectPin2);
-//    Serial.println("A1 changed");
-//    delay(10);
-//  }
-//  if (dataChanged(prevAux2Position, aux2Position))
-//  {
-//    setDac(aux2Position,1,dacChipSelectPin2);
-//    Serial.println("A2 changed");
-//    delay(10);
-//  }
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (dataChanged(prevSteerPosition,steerPosition) || dataChanged(prevThrottlePosition,throttlePosition))
+  {
+    DAC1.Set(steerPosition,throttlePosition);
+    Serial.println("DAC1 set");
+    delay(10);
+  }
 
+  if (dataChanged(prevAux1Position,aux1Position) || dataChanged(prevAux2Position,aux2Position))
+  {
+    DAC2.Set(aux1Position,aux2Position);
+    Serial.println("DAC2 set");
+    delay(10);
+  }
 }
 
-// callback for received data
+
+// Callback for received data from RPi
 void receiveData(int byteCount)
  {
+    // We expect 5 bytes to return from RPi
     if(Wire.available()>=5) 
     {
+        // Command byte (not implemented as of now)
         cmd = Wire.read();
+
+        // Store previous values
         prevSteerPosition = steerPosition;
         prevThrottlePosition = throttlePosition;
         prevAux1Position = aux1Position;
         prevAux2Position = aux2Position;
-        steerPosition = Wire.read();
-        throttlePosition = Wire.read();
-        aux1Position = Wire.read();
-        aux2Position = Wire.read();
-        // print al values
-        steerPosition = map(steerPosition,minimum,maximum,0,4095);
-        throttlePosition = map(throttlePosition,maximum,minimum,0,4095);
-        aux1Position = map(aux1Position,maximum,minimum,0,4095);
-        aux2Position = map(aux2Position,maximum,minimum,0,4095);
+
+        // Get new values
+        steerPosition = map(Wire.read(),minimum,maximum,dacMinimum,dacMaximum);
+        throttlePosition = map(Wire.read(),minimum,maximum,dacMinimum,dacMaximum);
+        aux1Position = map(Wire.read(),minimum,maximum,dacMinimum,dacMaximum);
+        aux2Position = map(Wire.read(),minimum,maximum,dacMinimum,dacMaximum);
+        
+        // Print all values (for testing)
         // Serial.print(cmd); Serial.print("  "); Serial.print(steerPosition); Serial.print("  "); Serial.print(throttlePosition); Serial.print("  "); Serial.print(aux1Position); Serial.print("  "); Serial.println(aux2Position);
     }
 
@@ -94,38 +89,5 @@ void receiveData(int byteCount)
 
 bool dataChanged(int prevValue, int value)
 {
-  if(abs(prevValue-value)>10)
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-}
-
-void setDac(int value, int channel, const int chipSelect)
-{
-  byte dacRegister = 0b00110000;
-  int dacSecondaryByteMask = 0b0000000011111111;
-  byte dacPrimaryByte = (value>>8)|dacRegister;
-  byte dacSecondaryByte = value & dacSecondaryByteMask;
-  switch(channel)
-  {
-    case 0:
-      dacPrimaryByte &= 127;
-    break;
-    case 1:
-      dacPrimaryByte |= (1<<7);
-    break;
-  }
-
-  
-  //noInterrupts();
-  digitalWrite(chipSelect,LOW);
-  SPI.transfer(dacPrimaryByte);
-  SPI.transfer(dacSecondaryByte);
-  digitalWrite(chipSelect,HIGH);
-  //interrupts();
-  
+  if(abs(prevValue-value)>dataThreshold) return true; else return false;
 }
